c222e3eba724e7cc4013cc97b0e9458293285de4
Johan Karlsson <DonKarlssonSan@gmail.com>
2016-05-18 13:34:33 +0200

    Extracting functions

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,23 +1,34 @@
 var add = function (numberString) {
   if(numberString.length > 0) {
     var extraDelimiter = [];
-    if(numberString.startsWith("//")) {
+    if(hasDelimiterSpecified(numberString)) {
       extraDelimiter.push(numberString[2]);
-      // Remove first line
-      var temp = numberString.split("\n");
-      temp.splice(0, 1);
-      numberString = temp.join("");
+      numberString = removeFirstLine(numberString);
     }
     var delimiters = [",", "\n"].concat(extraDelimiter);
-    var regexp = new RegExp("[" + delimiters.join("") + "]");
-    var sum = numberString
-      .split(regexp)
-      .map(num => parseInt(num))
-      .reduce((s, prev) => s + prev);
-    return sum;
+    return getSum(numberString, delimiters);
   }
 
   return 0;
+}
+
+function hasDelimiterSpecified(numberString) {
+  return numberString.startsWith("//");
+}
+
+function removeFirstLine(numberString) {
+    var temp = numberString.split("\n");
+    temp.splice(0, 1);
+    return temp.join("");
+}
+
+function getSum(numberString, delimiters) {
+  var regexp = new RegExp("[" + delimiters.join("") + "]");
+  var sum = numberString
+    .split(regexp)
+    .map(num => parseInt(num))
+    .reduce((s, prev) => s + prev);
+  return sum;
 }
 
 exports.add = add;
